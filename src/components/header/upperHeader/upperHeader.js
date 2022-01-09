e2d9c78de786f7373e2699663bed32f59e5ebff1
Arash <arash.mehdiizadeh@gmail.com>
2022-01-10 00:55:29 +0330

    fixed a missing tag

--- src/components/header/upperHeader/upperHeader.js
+++ src/components/header/upperHeader/upperHeader.js
@@ -12,46 +12,40 @@ const UpperHeader = ({shadow}) => {
     const shadowdata = "0 7px 8px 0 rgb(0 0 0 / 0.4)";
     
     return (
-        <Router>
-            <div className="upperHeader-container" style={{boxShadow: shadow ? shadowdata : ""}} > {/* style={isShow ? { display: 'none' } : {} } */}
-                <div className='search-wrapper'>
-                    <div className="logo-container">
-                        <a href="https://www.digikala.com/" className="logo" alt="دیجی کالا">
-                            <DigikalaIco />
-                        </a>
-                    </div>
-
-                    <div className="search-container">
-                        {/*<span class="fa fa-search form-control-feedback"></span>*/}
-                        <input type="search" className='searchBox'
-                        /*  ${isActive ? 'isActiveSearch' : ""}`} onClick={() => setIsActive(!isActive) */
-                        placeholder="جستجو در دیجی&zwnj;کالا …"
-                        dir="rtl" align="right"  />
-                    </div>
+        
+        <div className="upperHeader-container" style={{boxShadow: shadow ? shadowdata : ""}} > {/* style={isShow ? { display: 'none' } : {} } */}
+            <div className='search-wrapper'>
+                <div className="logo-container">
+                    <a href="https://www.digikala.com/" className="logo" alt="دیجی کالا">
+                        <DigikalaIco />
+                    </a>
                 </div>
-                        
-                        
-                    <div className="login-container">
-                        
-                        <div path={"/users/login-register"} component={<Login/>}  className="loginSection">
-
-                            <div className="userPic-container">
-                                <img src={UserPic} className="userPic" alt=""></img>
-                            </div>
-                            <div  className="userText-container">
-                                <p className="usertext">ورود به حساب کاربری</p>
-                            </div>
-                            
-                        </div>
-                        <div className="vertical"></div>
-
-                        <a href="/cart" className="shoppingCart" >
-                            <ShoppingCart />
-                        </a>
-                    </div>    
+                <div className="search-container">
+                    {/*<span class="fa fa-search form-control-feedback"></span>*/}
+                    <input type="search" className='searchBox'
+                    /*  ${isActive ? 'isActiveSearch' : ""}`} onClick={() => setIsActive(!isActive) */
+                    placeholder="جستجو در دیجی&zwnj;کالا …"
+                    dir="rtl" align="right"  />
                 </div>
+            </div>
                 
-            </Router>
+            <div className="login-container">
+                
+                <div className="loginSection">
+                    <div className="userPic-container">
+                        <img src={UserPic} className="userPic" alt=""></img>
+                    </div>
+                    <div  className="userText-container">
+                        <p className="usertext">ورود به حساب کاربری</p>
+                    </div>
+                    
+                </div>
+                <div className="vertical"></div>
+                <a href="/cart" className="shoppingCart" >
+                    <ShoppingCart />
+                </a>
+            </div>    
+        </div>
         )
 }
 
